4f6d9ae4967861aba3017979f19464ffd7bba4e9
Karishma <karishma1997.s@gmail.com>
2022-10-26 19:09:02 +0530

    Minor changes in iterators

--- Week-4/Exercise4-3.js
+++ Week-4/Exercise4-3.js
@@ -1,9 +1,9 @@
-let n1=0, n2=1, number = 6, temp;
+let n1=0, n2=1, number = [1,2,3,4,5,6,7], temp;
 console.log("Fibonnaci series");
-while(number >=0){
+for (let n in number){
     console.log(n1);
     temp=n1+n2;
     n1=n2;
     n2=temp;
-    number--;
+    n--;
 }
